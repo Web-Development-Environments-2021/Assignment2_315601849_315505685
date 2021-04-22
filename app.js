931092d002c0668f7f5c2e0a91fef4b26383c906
shakedal <salmog510@gmail.com>
2021-04-22 10:58:42 +0300

    add login

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var start_time;
 var time_elapsed;
 var interval;
 var users = { k: { password: "k" } };
+var logged_in_user = null;
 
 function show_only_button(button_text) {
   $(".div-content").hide();
@@ -16,113 +17,11 @@ function show_only_button(button_text) {
 $(document).ready(function () {
   context = canvas.getContext("2d");
   $("#welcome").show();
+  $("#welcome-greeting").hide();
   setMaxDate();
   StartGame();
 });
 
-function setMaxDate() {
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth() + 1; //January is 0!
-  var yyyy = today.getFullYear();
-  if (dd < 10) {
-    dd = "0" + dd;
-  }
-  if (mm < 10) {
-    mm = "0" + mm;
-  }
-
-  today = yyyy + "-" + mm + "-" + dd;
-  document.getElementById("datefield").setAttribute("max", today);
-}
-
-function register() {
-  let valid = true;
-  let error = "";
-
-  let username = $("#username-reg").val();
-  if (userExist(username) || username.length == 0) {
-    setError("#username-reg");
-    error += "Please choose a different username, this one is taken :)\n";
-    valid = false;
-  } else {
-    cleanError("#username-reg");
-  }
-
-  let password = $("#pass-reg").val();
-  if (!validPass(password)) {
-    setError("#pass-reg");
-    error += "Please choose a valid password.\n";
-    valid = false;
-  } else {
-    cleanError("#pass-reg");
-  }
-
-  let name = $("#name-reg").val();
-  if (!name.match(/^([\w]{1,})+\s+([\w\s]{1,})+$/i)) {
-    setError("#name-reg");
-    error += "Please insert your full name\n";
-    valid = false;
-  } else {
-    cleanError("#name-reg");
-  }
-
-  let email = $("#email-reg").val();
-  if (!isEmail(email)) {
-    setError("#email-reg");
-    error += "Please insert a valid email address\n";
-    valid = false;
-  } else {
-    cleanError("#email-reg");
-  }
-
-  let birthday = $("#datefield").val();
-
-  //finish validating
-  if (valid) {
-    users[username] = {
-      password: password,
-      full_name: name,
-      email: email,
-      birth_date: birthday,
-    };
-    alert("You have registered successfully.");
-    $("#reg-form").trigger("reset");
-    show_only_button("login");
-    return false;
-  } else {
-    alert(error);
-    return false;
-  }
-}
-
-function validPass(pass) {
-  if (pass.length < 6) {
-    return false;
-  }
-  let regex_pass = /^(?=.*\d)(?=.*[a-z|A-Z])/;
-  if (pass.match(regex_pass)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
-
-function userExist(username) {
-  return username in users;
-}
-
-function cleanError(field) {
-  $(field).css("box-shadow", "1px 1px 4px rgba(0, 0, 0, 0.3)");
-}
-
-function setError(field) {
-  $(field).css("box-shadow", "1px 1px 4px rgba(179, 16, 16, 1)");
-}
 
 function StartGame() {
   board = new Array();
---
+++ login.js
@@ -0,0 +1,23 @@
+
+function login(){
+    let username = $("#login-username").val();
+    let password = $("#login-password").val();
+
+    if(!userExist(username)){
+        alert("User doesn't exist")
+    }
+    else if(users[username].password != password){
+        alert("User doesn't exist")
+    }
+    else{
+        welcomeUser(username);
+    }
+    return false;
+}
+
+function welcomeUser(username){
+    show_only_button("welcome");
+    $(".welcome-button").hide();
+    $("#welcome-greeting").show();
+    $("#welcome-greeting").text("Welcome " + username +"!");
+}
