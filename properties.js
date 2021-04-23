001e718cabef781e067d09ce08133fd265b9cb2d
boublilm <45751927+boublilm@users.noreply.github.com>
2021-04-23 13:41:35 +0300

    commit changes

--- properties.js
+++ properties.js
@@ -1,13 +1,29 @@
-let left_key = 37;
-let right_key = 39;
-let up_key = 38;
-let down_key = 40;
-let number_of_balls = 50;
-let percentage_5_balls = 0.6;
-let color_5_balls = "red";
-let percentage_15_balls = 0.3;
-let color_15_balls = "blue";
-let percentage_25_balls = 0.1;
-let color_25_balls = "white";
-let game_time = 60;
-let num_of_monsters = 4;
+let left_key;
+let right_key;
+let up_key;
+let down_key;
+let number_of_balls;
+let percentage_5_balls;
+let color_5_balls;
+let percentage_15_balls;
+let color_15_balls;
+let percentage_25_balls;
+let color_25_balls;
+let game_time;
+let num_of_monsters;
+
+function set_default_properties() {
+  let left_key = 37;
+  let right_key = 39;
+  let up_key = 38;
+  let down_key = 40;
+  let number_of_balls = 50;
+  let percentage_5_balls = 0.6;
+  let color_5_balls = "red";
+  let percentage_15_balls = 0.3;
+  let color_15_balls = "blue";
+  let percentage_25_balls = 0.1;
+  let color_25_balls = "white";
+  let game_time = 60;
+  let num_of_monsters = 4;
+}
