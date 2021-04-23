b6b4360f3b5ff59bb274e3ea3d2e7943fa02c035
Maor Boublil <boublilm@post.bgu.ac.il>
2021-04-23 15:27:56 +0300

    Recording buttons

--- properties.js
+++ properties.js
@@ -11,6 +11,7 @@ let percentage_25_balls;
 let color_25_balls;
 let game_time;
 let num_of_monsters;
+let last_pressed;
 
 function set_default_properties() {
   left_key = 37;
@@ -28,10 +29,10 @@ function set_default_properties() {
   num_of_monsters = 4;
 }
 
-function show_rec_dialog() {
+async function show_rec_dialog(key) {
   $(".modal").show();
-  //$(".modal").hide();
-  return false;
+  last_pressed = key;
+  await waitingKeypress(key);
 }
 
 function hide_rec_dialog() {
@@ -42,3 +43,23 @@ function hide_rec_dialog() {
 $(document).ready(function () {
   hide_rec_dialog();
 });
+
+function waitingKeypress(key) {
+  return new Promise((resolve) => {
+    document.addEventListener("keydown", onKeyHandler);
+    function onKeyHandler(e) {
+      document.removeEventListener("keydown", onKeyHandler);
+      resolve();
+      if (last_pressed == "L") {
+        left_key = e.which;
+      } else if (last_pressed == "U") {
+        up_key = e.which;
+      } else if (last_pressed == "D") {
+        down_key = e.which;
+      } else if (last_pressed == "R") {
+        right_key = e.which;
+      }
+      $(".modal").hide();
+    }
+  });
+}
