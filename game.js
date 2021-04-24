7d2c90ab9f2c053c6f67a4ba865306b5d6ffb896
shakedal <salmog510@gmail.com>
2021-04-24 13:16:28 +0300

    candy with different points

--- game.js
+++ game.js
@@ -5,7 +5,13 @@ let candies_count = 0;
 function CreateBoardGame() {
     var cnt = board_width * board_hight;
     let wall_remain = 20;
-    var food_remain = number_of_balls;
+    //calculate number of balls
+    let food_5_remain = Math.floor(number_of_balls*percentage_5_balls);
+    let food_15_remain = Math.floor(number_of_balls*percentage_15_balls);
+    let food_25_remain = Math.floor(number_of_balls*percentage_25_balls);
+    let leftovers = number_of_balls - (food_5_remain+food_15_remain+food_25_remain);
+    food_5_remain += leftovers;
+
     var pacman_remain = 1;
     board = new Array();
 
@@ -14,20 +20,20 @@ function CreateBoardGame() {
         for (var j = 0; j < board_hight; j++) {
             var randomNum = Math.random();
             //Candy
-            if (randomNum <= (1.0 * food_remain) / cnt) {
-                let points = 15;
-                food_remain--;
-                board[i][j] = new Candy(i, j, points);
+            if (randomNum <= (1.0 * food_5_remain) / cnt) {
+                //let points = 5;
+                food_5_remain--;
+                board[i][j] = new Candy(i, j, 5);
                 candies_count++;
             }
             //Pacman
-            else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
+            else if (randomNum < (1.0 * (pacman_remain + food_5_remain)) / cnt) {
                 pacman_obj = new Pacman(i, j);
                 pacman_remain--;
                 board[i][j] = pacman_obj;
             }
             //Set Obstacles
-            else if (randomNum <= (1.0 * (wall_remain + pacman_remain + food_remain)) / cnt){
+            else if (randomNum <= (1.0 * (wall_remain + pacman_remain + food_5_remain)) / cnt){
                 wall_remain--;
                 board[i][j] = new Wall(i, j);
             }
@@ -41,14 +47,33 @@ function CreateBoardGame() {
         }
     }
 
-    while (food_remain > 0) {
+    while (food_5_remain > 0) {
         var emptyCell = findRandomEmptyCell(board);
         let i = emptyCell[0];
         let j = emptyCell[1];
-        let points = 15;
-        board[i][j] = new Candy(i, j, points);
-        food_remain--;
-        //candies_array[candies_count] = new Candy(emptyCell[0],emptyCell[1], points);
+        //let points = 5;
+        board[i][j] = new Candy(i, j, 5);
+        food_5_remain--;
+        candies_count++;
+    }
+
+    while (food_15_remain > 0) {
+        var emptyCell = findRandomEmptyCell(board);
+        let i = emptyCell[0];
+        let j = emptyCell[1];
+        //let points = 15;
+        board[i][j] = new Candy(i, j, 15);
+        food_15_remain--;
+        candies_count++;
+    }
+
+    while (food_25_remain > 0) {
+        var emptyCell = findRandomEmptyCell(board);
+        let i = emptyCell[0];
+        let j = emptyCell[1];
+        //let points = 25;
+        board[i][j] = new Candy(i, j, 25);
+        food_25_remain--;
         candies_count++;
     }
 
@@ -64,6 +89,11 @@ function findRandomEmptyCell(board) {
         j = Math.floor(Math.random() * 9 + 1);
     }
     return [i, j];
+}
+
+function getPoints(){
+    let points = Math.floor(Math.random() * 3)*10 +5;
+    return points;
 }
 
 
@@ -144,7 +174,7 @@ function UpdatePosition() {
     let cur_obj = board[pacman_obj.x][pacman_obj.y];
     if (cur_obj != null && cur_obj.constructor.name == "Candy") {
         score += cur_obj.points;
-
+        candies_count--;
     }
 
     //Place Packman in new position in array
@@ -156,8 +186,9 @@ function UpdatePosition() {
     if (score >= 100 && time_elapsed <= 10) {
         pac_color = "#D17A22";
     }
-    if (score == 50) {
-        window.clearInterval(interval);
+    if (candies_count == 0) {
+        Draw();
+        window.clearInterval(interval);  
         window.alert("Game completed");
     } else {
         Draw();
