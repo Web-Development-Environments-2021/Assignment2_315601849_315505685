0addfa450bc8275fdf3f77c38c56b63840dcba51
shakedal <salmog510@gmail.com>
2021-04-24 13:16:42 +0300

    candy with different points

--- candy.js
+++ candy.js
@@ -3,28 +3,14 @@ class Candy{
         this.x = x;
         this.y = y;
         this.points = points
+        if (points == 5){
+            this.radius = 10;
+        } else if (points == 25){
+            this.radius = 20;
+        } else{
+            this.radius = 15;
+        }
     }
-
-    /*
-    drawMe(){
-        let center = new Object();
-        center.x = this.x * 60 + 30;
-        center.y = this.y * 60 + 30;
-
-        context.beginPath();
-        context.arc(center.x, center.y, this.points, 0, 2 * Math.PI); // circle - radius equal to points
-        if(this.points == 5){
-            context.fillStyle = color_5_balls;
-        }
-        else if(this.points == 15){
-            context.fillStyle = color_15_balls;
-        }
-        else if(this.points == 25){
-            context.fillStyle = color_25_balls;
-        }
-        context.fill();
-    }
-    */
 
     drawMe(){
         let center = new Object();
@@ -32,7 +18,7 @@ class Candy{
         center.y = this.y * cell_size * 2 + cell_size;
 
         context.beginPath();
-        context.arc(center.x, center.y, this.points/30 * cell_size, 0, 2 * Math.PI); // circle - radius equal to points
+        context.arc(center.x, center.y, this.radius/30 * cell_size, 0, 2 * Math.PI); // circle - radius equal to points
         if(this.points == 5){
             context.fillStyle = color_5_balls;
         }
